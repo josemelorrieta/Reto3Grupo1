6bfb03ee2d1d0c6bfd07112119e3e70669d18c9c
IN1DM3B_10 <IN1DM3B_10@H015112.NET>
2019-01-16 14:08:50 +0100

    Agragados paquetes de src y test

---
+++ src/test/java/testsModelo/TestModelo.java
@@ -0,0 +1,14 @@
+package testsModelo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestModelo {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/java/testsVista/TestVista.java
@@ -0,0 +1,5 @@
+package testsVista;
+
+public class TestVista {
+
+}
