615da7ccd09bf8adc6fbab65987646fc32102bdb
IN1DM3B_15 <IN1DM3B_15@H015112.NET>
2019-01-21 09:32:28 +0100

    PanelParadas y PanelFechas terminado

--- src/main/java/vista/PanelParadas.java
+++ src/main/java/vista/PanelParadas.java
@@ -1,26 +1,25 @@
 package vista;
 
-import javax.swing.JPanel;
 import java.awt.Color;
-import javax.swing.border.LineBorder;
-import javax.swing.JLabel;
-import com.jgoodies.forms.factories.DefaultComponentFactory;
-import javax.swing.SwingConstants;
 import java.awt.Font;
+
 import javax.swing.JButton;
-import javax.swing.JRadioButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.LineBorder;
+
+import com.jgoodies.forms.factories.DefaultComponentFactory;
 import javax.swing.JComboBox;
 import javax.swing.JTextArea;
+import javax.swing.JList;
 
 public class PanelParadas extends JPanel {
 	
 	public JButton btnCancelar = new JButton("Cancelar");
 	public JButton btnSiguiente = new JButton("Siguiente");
-	public JComboBox FechaDeIda = new JComboBox();
-	public JComboBox FechaDeVuelta = new JComboBox();
-	public JRadioButton rdbtnNewRadioButton = new JRadioButton("Comprar billete de vuelta");
-	JTextArea textPrecio = new JTextArea();
-	private final JTextArea textLinea = new JTextArea();
+	public JComboBox ParadaDeOrigen = new JComboBox();
+	public JComboBox ParadaDeDestino = new JComboBox();
+	private final JTextArea textArea = new JTextArea();
 
 	/**
 	 * Create the panel.
@@ -29,52 +28,33 @@ public class PanelParadas extends JPanel {
 		setBorder(new LineBorder(new Color(0, 0, 0), 4));
 		setBackground(Color.CYAN);
 		setLayout(null);
-		
-		
+
 		btnCancelar.setFont(new Font("Tahoma", Font.BOLD, 20));
-		btnCancelar.setBounds(29, 627, 130, 49);
+		btnCancelar.setBounds(36, 640, 121, 33);
 		add(btnCancelar);
 		
-		
 		btnSiguiente.setFont(new Font("Tahoma", Font.BOLD, 20));
-		btnSiguiente.setBounds(935, 627, 130, 49);
+		btnSiguiente.setBounds(939, 640, 129, 33);
 		add(btnSiguiente);
+
+		JLabel lblParadaDeOrigen = DefaultComponentFactory.getInstance().createLabel("Parada de Origen:");
+		lblParadaDeOrigen.setFont(new Font("Tahoma", Font.PLAIN, 40));
+		lblParadaDeOrigen.setBounds(131, 262, 352, 49);
+		add(lblParadaDeOrigen);
 		
-		JLabel lblFechaDeIda = DefaultComponentFactory.getInstance().createLabel("Fecha de Ida:");
-		lblFechaDeIda.setFont(new Font("Tahoma", Font.PLAIN, 40));
-		lblFechaDeIda.setBounds(129, 255, 252, 49);
-		add(lblFechaDeIda);
-		
-		JLabel lblNewJgoodiesLabel = DefaultComponentFactory.getInstance().createLabel("Fecha de Vuelta:");
+		JLabel lblNewJgoodiesLabel = DefaultComponentFactory.getInstance().createLabel("Parada de Destino:");
 		lblNewJgoodiesLabel.setFont(new Font("Tahoma", Font.PLAIN, 40));
-		lblNewJgoodiesLabel.setBounds(646, 261, 314, 37);
+		lblNewJgoodiesLabel.setBounds(628, 268, 336, 37);
 		add(lblNewJgoodiesLabel);
 		
+		ParadaDeOrigen.setBounds(131, 322, 336, 49);
+		add(ParadaDeOrigen);
 		
-		rdbtnNewRadioButton.setHorizontalAlignment(SwingConstants.CENTER);
-		rdbtnNewRadioButton.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		rdbtnNewRadioButton.setBounds(409, 196, 224, 48);
-		add(rdbtnNewRadioButton);
+		ParadaDeDestino.setBounds(628, 316, 351, 49);		
+		add(ParadaDeDestino);
 		
-		
-		FechaDeIda.setBounds(129, 312, 252, 48);
-		add(FechaDeIda);
-		
-		
-		FechaDeVuelta.setBounds(646, 309, 296, 51);
-		add(FechaDeVuelta);
-		
-		JLabel lblPrecio = DefaultComponentFactory.getInstance().createLabel("Precio:");
-		lblPrecio.setFont(new Font("Tahoma", Font.PLAIN, 40));
-		lblPrecio.setBounds(299, 470, 138, 37);
-		add(lblPrecio);
-		
-		
-		textPrecio.setBounds(431, 470, 199, 37);
-		add(textPrecio);
-		textLinea.setBounds(129, 39, 831, 62);
-		
-		add(textLinea);
-
+		textArea.setBounds(131, 40, 848, 62);
+		add(textArea);
 	}
+
 }
