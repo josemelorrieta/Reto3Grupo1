e146d5ca4a33fe8a8f211e56705504d9ab23d386
IN1DM3B_10 <IN1DM3B_10@H015112.NET>
2019-01-21 09:42:43 +0100

    Metodo escribir a fichero sin Test -ni idea de como testear-

--- src/main/java/modelo/ConexionFichero.java
+++ src/main/java/modelo/ConexionFichero.java
@@ -2,6 +2,8 @@ package modelo;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
 
 public class ConexionFichero {
 	
@@ -40,6 +42,66 @@ public class ConexionFichero {
 		}
 		
 		return datos;
+	}
+	
+	public void imprimirBillete(String[] datosBillete) {
+		/* datos Billete = "Linea", "Origen", "Destino", "Fecha ida", "Fecha vuelta" , "DNI", "Nombre", "Apellidos", "Fecha nacimiento", "Sexo", "Fecha compra" */
+		FileWriter fichero = null;	
+		BufferedWriter linea = null;
+		
+		try {
+			fichero = new FileWriter("c:/Temp/" + datosBillete[5] + "-" + datosBillete[3].replaceAll("/",  "") + ".txt");
+			linea = new BufferedWriter(fichero);
+			
+			linea.write("BILLETE");
+			linea.newLine();
+			linea.write("-------");
+			linea.newLine();
+			linea.newLine();
+			linea.write("DATOS DEL TRAYECTO");
+			linea.newLine();
+			linea.write("------------------");
+			linea.newLine();
+			linea.write("Línea: " + datosBillete[0]);
+			linea.newLine();
+			linea.write("Origen: " + datosBillete[1]);
+			linea.newLine();
+			linea.write("Destino: " + datosBillete[2]);
+			linea.newLine();
+			linea.write("Fecha ida: " + datosBillete[3]);
+			linea.newLine();
+			if (datosBillete[4] != null) {
+				linea.write("Fecha vuelta: " + datosBillete[4]);
+				linea.newLine();
+			}
+			linea.newLine();
+			linea.write("DATOS DEL CLIENTE");
+			linea.newLine();
+			linea.write("-----------------");
+			linea.newLine();
+			linea.write("DNI: " + datosBillete[5]);
+			linea.newLine();
+			linea.write("Nombre y Apellidos: " + datosBillete[6] + " " + datosBillete[7]);
+			linea.newLine();
+			linea.write("Fecha nacimiento: " + datosBillete[8]);
+			linea.newLine();
+			linea.write("Sexo: " + datosBillete[9]);
+			linea.newLine();
+			linea.newLine();
+			linea.write("Fecha de la compra: " + datosBillete[10]);
+			linea.flush();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (fichero != null)
+					fichero.close();
+				if (linea != null)
+					linea.close();
+				} catch (Exception e) {
+					e.getStackTrace();
+			}
+		}
 	}
 	
 	
