46f9b569992ca3172e9436daff4f5273ce0674b0
IN1DM3B_01 <IN1DM3B_01@H015112.NET>
2019-01-25 12:37:33 +0100

    Metodo para crear nombres de lineas

--- src/test/java/testsControlador/FuncionesVariasTest.java
+++ src/test/java/testsControlador/FuncionesVariasTest.java
@@ -11,7 +11,7 @@ public class FuncionesVariasTest {
 	private FuncionesVarias funcionesTest = new FuncionesVarias();
 
 	@Test
-	public void testDistanciaEuclidea() throws Exception {
+	public void testComprobarDNI() throws Exception {
 		String DNI = "45899844Y";
 		String DNI2 = "45894Y";
 		
@@ -19,4 +19,10 @@ public class FuncionesVariasTest {
 		assertEquals(funcionesTest.comprobarDNI(DNI2), false);
 	}
 
+	
+	@Test
+	public void testCargarLineas() throws Exception {
+		System.out.println(funcionesTest.CargarLineas());
+		assertEquals(funcionesTest.CargarLineas(), "Termibus-Bilbao - Asilo Barrika");
+	}
 }
