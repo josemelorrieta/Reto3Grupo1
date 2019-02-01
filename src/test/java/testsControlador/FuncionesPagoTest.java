ac9b2e150997977bb6f1c9c38f8710940e3bfed7
IN1DM3B_15 <IN1DM3B_15@H015112.NET>
2019-02-01 14:24:35 +0100

    test pado y devolucion terminado

--- src/test/java/testsControlador/FuncionesPagoTest.java
+++ src/test/java/testsControlador/FuncionesPagoTest.java
@@ -14,7 +14,7 @@ public class FuncionesPagoTest {
 	
 	private Modelo miModelo = new Modelo();
 	private FuncionesPago pagoTest = new FuncionesPago();
-	private FuncionesDevolucion cambiosTest = new FuncionesDevolucion(miModelo);
+	private FuncionesDevolucion funcionesTest = new FuncionesDevolucion(miModelo);
 	//private FuncionesPago leerDatosMock = mock(FuncionesPago.class);
 	
 	
@@ -31,7 +31,7 @@ public class FuncionesPagoTest {
 	@Test
 	public void testCambiosMayor500() {
 		float dinero = 523f;
-		int [] cambiosTest = cambiosTest.(dinero);
+		int [] cambiosTest = funcionesTest.cambios(dinero);
 		
 		assertEquals(1, cambiosTest[0], 0.0001);
 		assertNotEquals(11f, cambiosTest[0], 0.0001);
@@ -40,7 +40,7 @@ public class FuncionesPagoTest {
 	@Test
 	public void testCambiosIgual500() {
 		float dinero = 500f;
-		int [] cambiosTest = pagoTest.Cambios(dinero);
+		int [] cambiosTest = funcionesTest.cambios(dinero);
 		
 		assertEquals(1, cambiosTest[0], 0.0001);
 		assertNotEquals(11f, cambiosTest[0], 0.0001); 
@@ -49,7 +49,7 @@ public class FuncionesPagoTest {
 	@Test
 	public void testCambiosMayor200() {
 		float dinero = 325f;
-		int [] cambiosTest = pagoTest.Cambios(dinero);
+		int [] cambiosTest = funcionesTest.cambios(dinero);
 		
 		assertEquals(1, cambiosTest[1], 0.0001);
 		assertNotEquals(11f, cambiosTest[1], 0.0001);
@@ -58,7 +58,7 @@ public class FuncionesPagoTest {
 	@Test
 	public void testCambiosIgual200() {
 		float dinero = 300f;
-		int [] cambiosTest = pagoTest.Cambios(dinero);
+		int [] cambiosTest = funcionesTest.cambios(dinero);
 		
 		assertEquals(1, cambiosTest[1], 0.0001);
 		assertNotEquals(11f, cambiosTest[1], 0.0001);
@@ -67,7 +67,7 @@ public class FuncionesPagoTest {
 	@Test
 	public void testCambiosMayor100() {
 		float dinero = 120f;
-		int [] cambiosTest = pagoTest.Cambios(dinero);
+		int [] cambiosTest = funcionesTest.cambios(dinero);
 		
 		assertEquals(1, cambiosTest[2], 0.0001);
 		assertNotEquals(11f, cambiosTest[2], 0.0001);
@@ -76,7 +76,7 @@ public class FuncionesPagoTest {
 	@Test
 	public void testCambiosIgual100() {
 		float dinero = 100f;
-		int [] cambiosTest = pagoTest.Cambios(dinero);
+		int [] cambiosTest = funcionesTest.cambios(dinero);
 		
 		assertEquals(1, cambiosTest[2], 0.0001);
 		assertNotEquals(11f, cambiosTest[2], 0.0001);
@@ -85,7 +85,7 @@ public class FuncionesPagoTest {
 	@Test
 	public void testCambiosMayor50() {
 		float dinero = 75f;
-		int [] cambiosTest = pagoTest.Cambios(dinero);
+		int [] cambiosTest = funcionesTest.cambios(dinero);
 		
 		assertEquals(1, cambiosTest[3], 0.0001);
 		assertNotEquals(11f, cambiosTest[3], 0.0001);
@@ -94,7 +94,7 @@ public class FuncionesPagoTest {
 	@Test
 	public void testCambiosIgual50() {
 		float dinero = 50f;
-		int [] cambiosTest = pagoTest.Cambios(dinero);
+		int [] cambiosTest = funcionesTest.cambios(dinero);
 		
 		assertEquals(1, cambiosTest[3], 0.0001);
 		assertNotEquals(11f, cambiosTest[3], 0.0001);
@@ -103,7 +103,7 @@ public class FuncionesPagoTest {
 	@Test
 	public void testCambiosMayor20() {
 		float dinero = 35f;
-		int [] cambiosTest = pagoTest.Cambios(dinero);
+		int [] cambiosTest = funcionesTest.cambios(dinero);
 		
 		assertEquals(1, cambiosTest[4], 0.0001);
 		assertNotEquals(11f, cambiosTest[4], 0.0001);
@@ -112,7 +112,7 @@ public class FuncionesPagoTest {
 	@Test
 	public void testCambiosIgual20() {
 		float dinero = 20f;
-		int [] cambiosTest = pagoTest.Cambios(dinero);
+		int [] cambiosTest = funcionesTest.cambios(dinero);
 		
 		assertEquals(1, cambiosTest[4], 0.0001);
 		assertNotEquals(11f, cambiosTest[4], 0.0001);
@@ -121,7 +121,7 @@ public class FuncionesPagoTest {
 	@Test
 	public void testCambiosMayor10() {
 		float dinero = 15f;
-		int [] cambiosTest = pagoTest.Cambios(dinero);
+		int [] cambiosTest = funcionesTest.cambios(dinero);
 		
 		assertEquals(1, cambiosTest[5], 0.0001);
 		assertNotEquals(11f, cambiosTest[5], 0.0001);
@@ -130,7 +130,7 @@ public class FuncionesPagoTest {
 	@Test
 	public void testCambiosIgual10() {
 		float dinero = 10f;
-		int [] cambiosTest = pagoTest.Cambios(dinero);
+		int [] cambiosTest = funcionesTest.cambios(dinero);
 		
 		assertEquals(1, cambiosTest[5], 0.0001);
 		assertNotEquals(11f, cambiosTest[5], 0.0001);
@@ -139,7 +139,7 @@ public class FuncionesPagoTest {
 	@Test
 	public void testCambiosMayor5() {
 		float dinero = 7f;
-		int [] cambiosTest = pagoTest.Cambios(dinero);
+		int [] cambiosTest = funcionesTest.cambios(dinero);
 		
 		assertEquals(1, cambiosTest[6], 0.0001);
 		assertNotEquals(11f, cambiosTest[6], 0.0001);
@@ -148,7 +148,7 @@ public class FuncionesPagoTest {
 	@Test
 	public void testCambiosIgual5() {
 		float dinero = 5f;
-		int [] cambiosTest = pagoTest.Cambios(dinero);
+		int [] cambiosTest = funcionesTest.cambios(dinero);
 		
 		assertEquals(1, cambiosTest[6], 0.0001);
 		assertNotEquals(11f, cambiosTest[6], 0.0001);
@@ -157,7 +157,7 @@ public class FuncionesPagoTest {
 	@Test
 	public void testCambiosMayor2() {
 		float dinero = 3f;
-		int [] cambiosTest = pagoTest.Cambios(dinero);
+		int [] cambiosTest = funcionesTest.cambios(dinero);
 		
 		assertEquals(1, cambiosTest[7], 0.0001);
 		assertNotEquals(11f, cambiosTest[7], 0.0001);
@@ -166,7 +166,7 @@ public class FuncionesPagoTest {
 	@Test
 	public void testCambiosIgual2() {
 		float dinero = 2f;
-		int [] cambiosTest = pagoTest.Cambios(dinero);
+		int [] cambiosTest = funcionesTest.cambios(dinero);
 		
 		assertEquals(1, cambiosTest[7], 0.0001);
 		assertNotEquals(11f, cambiosTest[7], 0.0001);
@@ -175,7 +175,7 @@ public class FuncionesPagoTest {
 	@Test
 	public void testCambiosMayor1() {
 		float dinero = 1.5f;
-		int [] cambiosTest = pagoTest.Cambios(dinero);
+		int [] cambiosTest = funcionesTest.cambios(dinero);
 		
 		assertEquals(1, cambiosTest[8], 0.0001);
 		assertNotEquals(11f, cambiosTest[8], 0.0001);
@@ -184,7 +184,7 @@ public class FuncionesPagoTest {
 	@Test
 	public void testCambiosIgual1() {
 		float dinero = 1f;
-		int [] cambiosTest = pagoTest.Cambios(dinero);
+		int [] cambiosTest = funcionesTest.cambios(dinero);
 		
 		assertEquals(1, cambiosTest[8], 0.0001);
 		assertNotEquals(11f, cambiosTest[8], 0.0001);
@@ -193,7 +193,7 @@ public class FuncionesPagoTest {
 	@Test
 	public void testCambiosMayor50c() {
 		float dinero = 0.75f;
-		int [] cambiosTest = pagoTest.Cambios(dinero);
+		int [] cambiosTest =funcionesTest.cambios(dinero);
 		
 		assertEquals(1, cambiosTest[9], 0.0001);
 		assertNotEquals(11f, cambiosTest[9], 0.0001);
@@ -202,7 +202,7 @@ public class FuncionesPagoTest {
 	@Test
 	public void testCambiosIgual50c() {
 		float dinero = 0.50f;
-		int [] cambiosTest = pagoTest.Cambios(dinero);
+		int [] cambiosTest = funcionesTest.cambios(dinero);
 		
 		assertEquals(1, cambiosTest[9], 0.0001);
 		assertNotEquals(11f, cambiosTest[9], 0.0001);
@@ -211,7 +211,7 @@ public class FuncionesPagoTest {
 	@Test
 	public void testCambiosMayor20c() {
 		float dinero = 0.30f;
-		int [] cambiosTest = pagoTest.Cambios(dinero);
+		int [] cambiosTest = funcionesTest.cambios(dinero);
 		
 		assertEquals(1, cambiosTest[10], 0.0001);
 		assertNotEquals(11f, cambiosTest[10], 0.0001);
@@ -220,7 +220,7 @@ public class FuncionesPagoTest {
 	@Test
 	public void testCambiosIgual20c() {
 		float dinero = 0.20f;
-		int [] cambiosTest = pagoTest.Cambios(dinero);
+		int [] cambiosTest = funcionesTest.cambios(dinero);
 		
 		assertEquals(1, cambiosTest[10], 0.0001);
 		assertNotEquals(11f, cambiosTest[10], 0.0001);
@@ -229,7 +229,7 @@ public class FuncionesPagoTest {
 	@Test
 	public void testCambiosMayor10c() {
 		float dinero = 0.15f;
-		int [] cambiosTest = pagoTest.Cambios(dinero);
+		int [] cambiosTest = funcionesTest.cambios(dinero);
 		
 		assertEquals(1, cambiosTest[11], 0.0001);
 		assertNotEquals(11f, cambiosTest[11], 0.0001);
@@ -238,7 +238,7 @@ public class FuncionesPagoTest {
 	@Test
 	public void testCambiosIgual10c() {
 		float dinero = 0.10f;
-		int [] cambiosTest = pagoTest.Cambios(dinero);
+		int [] cambiosTest = funcionesTest.cambios(dinero);
 		
 		assertEquals(1, cambiosTest[11], 0.0001);
 		assertNotEquals(11f, cambiosTest[11], 0.0001);
@@ -247,7 +247,7 @@ public class FuncionesPagoTest {
 	@Test
 	public void testCambiosMayor5c() {
 		float dinero = 0.07f;
-		int [] cambiosTest = pagoTest.Cambios(dinero);
+		int [] cambiosTest = funcionesTest.cambios(dinero);
 		
 		assertEquals(1, cambiosTest[12], 0.0001);
 		assertNotEquals(11f, cambiosTest[12], 0.0001);
@@ -256,7 +256,7 @@ public class FuncionesPagoTest {
 	@Test
 	public void testCambiosIgual5c() {
 		float dinero = 0.05f;
-		int [] cambiosTest = pagoTest.Cambios(dinero);
+		int [] cambiosTest = funcionesTest.cambios(dinero);
 		
 		assertEquals(1, cambiosTest[12], 0.0001);
 		assertNotEquals(11f, cambiosTest[12], 0.0001);
@@ -265,7 +265,7 @@ public class FuncionesPagoTest {
 	@Test
 	public void testCambiosMayor2c() {
 		float dinero = 0.03f;
-		int [] cambiosTest = pagoTest.Cambios(dinero);
+		int [] cambiosTest = funcionesTest.cambios(dinero);
 		
 		assertEquals(1, cambiosTest[13], 0.0001);
 		assertNotEquals(11f, cambiosTest[13], 0.0001);
@@ -274,7 +274,7 @@ public class FuncionesPagoTest {
 	@Test
 	public void testCambiosIgual2c() {
 		float dinero = 0.02f;
-		int [] cambiosTest = pagoTest.Cambios(dinero);
+		int [] cambiosTest = funcionesTest.cambios(dinero);
 		
 		assertEquals(1, cambiosTest[13], 0.0001);
 		assertNotEquals(11f, cambiosTest[13], 0.0001);
@@ -283,7 +283,7 @@ public class FuncionesPagoTest {
 	@Test
 	public void testCambiosIgual1c() {
 		float dinero = 0.01f;
-		int [] cambiosTest = pagoTest.Cambios(dinero);
+		int [] cambiosTest = funcionesTest.cambios(dinero);
 		
 		assertEquals(1, cambiosTest[14], 0.0001);
 		assertNotEquals(11f, cambiosTest[14], 0.0001);
