6c90094600297065acf8f886aca979e2a4b5c3a7
IN1DM3B_10 <IN1DM3B_10@H015112.NET>
2019-02-15 13:04:41 +0100

    Arreglar test de clases modificadas

--- src/test/java/testsModelo/FuncionesRegistroTest.java
+++ src/test/java/testsModelo/FuncionesRegistroTest.java
@@ -7,6 +7,7 @@ import java.sql.SQLException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 
 import org.junit.Test;
 
@@ -33,6 +34,7 @@ public class FuncionesRegistroTest {
 	
 	@Test
 	public void registrarNuevoCliente2() throws SQLException, ParseException {
+		dni = "11111112A";
 		cliente = func.registrarNuevoCliente(dni, nombre, apellidos, sexo, fechaNac, pass);
 		assertEquals(cliente.getSexo(), 'V');
 		System.out.println(cliente.getSexo());
@@ -63,57 +65,58 @@ public class FuncionesRegistroTest {
 	
 	@Test
 	public void comprobarCamposRegistroTest() throws SQLException {		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
 		
 		String dni = "12345678Z";	
 		String nombre = "osito";
 		String apellido = "gominola";
-		String fecha = "2000-08-25";
+		Date fecha = null;
+		try {
+			fecha = sdf.parse("2000-08-25");
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		char[] password = {1,2,3};
 		char[] password2 = {1,2,3, 4};
 		
 		String dni2 = "12345";	
 		String nombre2 = "123";
 		String apellido2 = "123";
-		String fecha2 = null;
+		Date fecha2 = null;
 		
 		String dni3 = "12345678Z";	
 		String nombre3 = "123";
 		String apellido3 = "123";
-		String fecha3 =  "2000-08-25";
 		
 		String dni4 = "1234567";	
 		String nombre4 = "123";
 		String apellido4 = "123";
-		String fecha4 =  "2000-08-25";
 		
 		String dni5 = "12345678Z";	
 		String nombre5 = "AA";
 		String apellido5 = "123";
-		String fecha5 =  "2000-08-25";
 		
 		String dni6 = "12345678Z";	
 		String nombre6 = "123";
 		String apellido6 = "BB";
-		String fecha6 =  "2000-08-25";
 		
 		String dni7 = null;	
 		String nombre7 = null;
 		String apellido7 = null;
-		String fecha7 = null;
 		
 		String dni8 = "78878943G";	
 		String nombre8 = "Otilio";
 		String apellido8 = "Ladrillo";
-		String fecha8 = "2000-08-25";
 		
 		assertEquals(func.comprobarCamposRegistro(dni, nombre, apellido, fecha, password, password),true);
 		assertEquals(func.comprobarCamposRegistro(dni2, nombre2, apellido2, fecha2, password, password), false);
-		assertEquals(func.comprobarCamposRegistro(dni3, nombre3, apellido3, fecha3, password, password), false);
-		assertEquals(func.comprobarCamposRegistro(dni4, nombre4, apellido4, fecha4, password, password), false);
-		assertEquals(func.comprobarCamposRegistro(dni5, nombre5, apellido5, fecha5, password, password), false);	
-		assertEquals(func.comprobarCamposRegistro(dni6, nombre6, apellido6, fecha6, password, password), false);
-		assertEquals(func.comprobarCamposRegistro(dni7, nombre7, apellido7, fecha7, password, password), false);
-		assertEquals(func.comprobarCamposRegistro(dni8, nombre8, apellido8, fecha8, password, password2), false);
+		assertEquals(func.comprobarCamposRegistro(dni3, nombre3, apellido3, fecha, password, password), false);
+		assertEquals(func.comprobarCamposRegistro(dni4, nombre4, apellido4, fecha, password, password), false);
+		assertEquals(func.comprobarCamposRegistro(dni5, nombre5, apellido5, fecha, password, password), false);	
+		assertEquals(func.comprobarCamposRegistro(dni6, nombre6, apellido6, fecha, password, password), false);
+		assertEquals(func.comprobarCamposRegistro(dni7, nombre7, apellido7, fecha, password, password), false);
+		assertEquals(func.comprobarCamposRegistro(dni8, nombre8, apellido8, fecha, password, password2), false);
 	
 	}
 
